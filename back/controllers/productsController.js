ab699fe382a72bf9e6bd9df8e31f4f23958f6692
Brejan <bspe1997@gmail.com>
2023-11-05 23:06:31 -0500

    Avance 2023-11-05 23:04

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,10 +1,28 @@
 const producto=require("../models/productos")  
 
 //Ver la lista de productos
-exports.getProducts=(req,res,next)=>{
+exports.getProducts=async(req,res,next)=>{
+const productos= await producto.find();
     res.status(200).json({
         success:true,
-        message: "En esta ruta usted va a poder ver todos los productos"
+        cantidad: productos.length,
+        productos
+    })
+}
+
+//Ver un producto por ID
+exports.getProductById= async (req,res,next)=>{
+    const product= await producto.findById(req.params.id)
+    if (!product){
+        return res.status(404).json({
+            success: false,
+            message: "No encontramos ese producto"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message: "Aqui debajo encuentras informacion sobre tu producto:",
+        product
     })
 }
 
--- back/routes/products.js
+++ back/routes/products.js
@@ -2,12 +2,14 @@ const express=require("express")
 const router=express.Router();
 
 //Traemos la respuesta json desde el controaldor
-const {getProducts, newProduct}= require("../controllers/productsController")
+const {getProducts, newProduct, getProductById}= require("../controllers/productsController")
 
 //Establecemos desde que ruta queremos ver el getProducts
 router.route('/productos').get(getProducts)
 //establecemos la ruta 
 router.route('/producto/nuevo').post(newProduct);
+//Ruta para consultar por Id
+router.route('/producto/:id').get(getProductById);
 
 module.exports=router;
 
